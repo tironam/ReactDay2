c38fd203fb1d5774fba4994f4ee3f7e6e58baddd
Michael Tirona <tironamichael@gmail.com>
2020-06-19 11:54:30 -0700

    Updates

---
+++ precache-manifest.18d5ba14dfdcbe0e0c62840d3e5a45c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daa251b6d7321f9498b18019becec15e",
+    "url": "/reactday2/index.html"
+  },
+  {
+    "revision": "48e72949a5b7a84e8d9c",
+    "url": "/reactday2/static/js/2.3f00af43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactday2/static/js/2.3f00af43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed3aab75aa47639f8f3b",
+    "url": "/reactday2/static/js/main.c3d36901.chunk.js"
+  },
+  {
+    "revision": "1bb6a36737e1c4ab86e7",
+    "url": "/reactday2/static/js/runtime-main.6efaba73.js"
+  }
+]);
